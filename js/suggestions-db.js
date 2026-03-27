e2515beaa37608edb53b56001aa6cf2d9633c1c1
inqupa <inqupa@gmail.com>
2026-03-28 01:58:48 +0530

    Update suggestions-db.js
    added new phrases

--- js/suggestions-db.js
+++ js/suggestions-db.js
@@ -1,5 +1,16 @@
+/**
+ * SUGGESTIONS-DB.JS - The Emotion Engine Data
+ */
 export const INLINE_PHRASES = {
-    "i feel": [" overwhelmed", " exhausted", " like I'm stuck", " lonely"],
-    "it's just": [" too much right now", " unfair", " hard to explain"],
-    "i wish": [" things were different", " I could sleep", " I didn't care"]
+    // Emotional Starters
+    "i feel": [" overwhelmed", " exhausted", " lonely", " anxious", " invisible"],
+    "i am": [" struggling", " tired of trying", " lost", " hurt", " frustrated"],
+    
+    // Thought Starters
+    "it's just": [" too much", " unfair", " hard to explain", " heavy"],
+    "i wish": [" things were different", " I could sleep", " I didn't care"],
+    
+    // Reaction Starters
+    "why am i": [" always like this", " so angry", " so sad for no reason"],
+    "nobody": [" understands", " listens", " cares", " sees me"]
 };
