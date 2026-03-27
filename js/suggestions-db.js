0118a930da37f7cadec2bb2b9a14d157ae4e0473
inqupa <inqupa@gmail.com>
2026-03-27 07:27:55 +0530

    Create suggestions-db.js

---
+++ js/suggestions-db.js
@@ -0,0 +1,5 @@
+export const INLINE_PHRASES = {
+    "i feel": [" overwhelmed", " exhausted", " like I'm stuck", " lonely"],
+    "it's just": [" too much right now", " unfair", " hard to explain"],
+    "i wish": [" things were different", " I could sleep", " I didn't care"]
+};
