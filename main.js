513fbbbb38322310ac7153e97cb068a2858a837e
inqupa <inqupa@gmail.com>
2026-03-28 17:15:42 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+import { VentBridge } from './js/integration/vent-bridge.js';
 
+/**
+ * MAIN.JS
+ * The Ignition: Starts the application.
+ */
+document.addEventListener('DOMContentLoaded', () => {
+    // Fire up the Bridge
+    VentBridge.init();
+});
