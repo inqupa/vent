4b1db80facd7ede559d5e3251ad3f48b7e57c3fc
inqupa <inqupa.gmail.com>
2026-04-05 17:14:10 +0530

    feat: forgot to add the layout bootloader in the previous commit

---
+++ subSystems/bootloaders/layout_init.js
@@ -0,0 +1,11 @@
+/**
+ * VENT BOOTLOADER: LAYOUT IGNITION
+ * Role: Ensuring the visual environment is ready before the UI is built.
+ */
+async function initializeLayoutSystem() {
+    if (window.LayoutBridge) {
+        // We sync the search theme from the registry
+        await window.LayoutBridge.syncStyles('search_layout');
+        console.log("Layout Bootloader: Layout System Ignited.");
+    }
+}
